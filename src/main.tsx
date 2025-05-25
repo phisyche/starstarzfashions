22b820295a5e43372cf2bfedc12d556df0438c21
Psi Phi <orpheuscrypt@gmail.com>
2025-05-25 17:41:08 +0300

    Update main.tsx
    Fix queryclient error

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,22 @@
 
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+// import { SupabaseProvider } from '@/context/SupabaseContext'
+// import { CartProvider } from '@/context/CartContext'
+// import { FavoritesProvider } from '@/context/FavoritesContext'
+// import { Toaster } from '@/components/ui/toaster'
+
+// createRoot(document.getElementById("root")!).render(
+//   <SupabaseProvider>
+//     <CartProvider>
+//       <FavoritesProvider>
+//         <App />
+//         <Toaster />
+//       </FavoritesProvider>
+//     </CartProvider>
+//   </SupabaseProvider>
+// );
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,13 +25,21 @@ import { CartProvider } from '@/context/CartContext'
 import { FavoritesProvider } from '@/context/FavoritesContext'
 import { Toaster } from '@/components/ui/toaster'
 
+// Import React Query dependencies
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a QueryClient instance
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
-  <SupabaseProvider>
-    <CartProvider>
-      <FavoritesProvider>
-        <App />
-        <Toaster />
-      </FavoritesProvider>
-    </CartProvider>
-  </SupabaseProvider>
+  <QueryClientProvider client={queryClient}>
+    <SupabaseProvider>
+      <CartProvider>
+        <FavoritesProvider>
+          <App />
+          <Toaster />
+        </FavoritesProvider>
+      </CartProvider>
+    </SupabaseProvider>
+  </QueryClientProvider>
 );
